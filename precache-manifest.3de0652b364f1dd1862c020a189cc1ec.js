07ba704bb8821e23bb859d6784cc0d1fa9f4c2dd
Richard Hunter <richard@richardhunter.co.uk>
2020-05-31 09:13:38 +0100

    Updates

---
+++ precache-manifest.3de0652b364f1dd1862c020a189cc1ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0c456ea696e328f6bb701005a2c426",
+    "url": "/animated-navigation-transmissions/index.html"
+  },
+  {
+    "revision": "1160544f58de1c1a3b03",
+    "url": "/animated-navigation-transmissions/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "d9356cbf5ca2c3c74398",
+    "url": "/animated-navigation-transmissions/static/css/main.d1b6cddb.chunk.css"
+  },
+  {
+    "revision": "1160544f58de1c1a3b03",
+    "url": "/animated-navigation-transmissions/static/js/2.8aa4e93b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/animated-navigation-transmissions/static/js/2.8aa4e93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9356cbf5ca2c3c74398",
+    "url": "/animated-navigation-transmissions/static/js/main.882707e5.chunk.js"
+  },
+  {
+    "revision": "037fe89c1fca389dc037",
+    "url": "/animated-navigation-transmissions/static/js/runtime-main.c49109f8.js"
+  }
+]);
